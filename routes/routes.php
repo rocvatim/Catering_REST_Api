b0b3423275d0400b691556c4eeacf10229832cae
Tschouten95 <tschouten95@outlook.com>
2023-04-09 14:39:05 +0200

    Routing

--- routes/routes.php
+++ routes/routes.php
@@ -5,9 +5,9 @@
 
 // Define routes here
 $router->get('/test', App\Controllers\IndexController::class . '@test');
-$router->get('/facilities', App\Controllers\FacilityController::class . '@allFacilitiesRequest');
-$router->get('/facilities/search', App\Controllers\FacilityController::class . '@searchFacilities');
-$router->get('/facilities/{id}', App\Controllers\FacilityController::class . '@facilityRequest');
-$router->post('/facilities', App\Controllers\FacilityController::class . '@createFacility');
-$router->patch('/facilities/{id}', App\Controllers\FacilityController::class . '@updateFacility');
-$router->delete('/facilities/{id}', App\Controllers\FacilityController::class . '@deleteFacility');
+$router->get('/facilities', App\Controllers\BaseController::class . '@allFacilitiesRequest');
+$router->get('/facilities/search', App\Controllers\BaseController::class . '@searchFacilities');
+$router->get('/facilities/{id}', App\Controllers\BaseController::class . '@facilityRequest');
+$router->post('/facilities', App\Controllers\BaseController::class . '@createFacility');
+$router->patch('/facilities/{id}', App\Controllers\BaseController::class . '@updateFacility');
+$router->delete('/facilities/{id}', App\Controllers\BaseController::class . '@deleteFacility');
