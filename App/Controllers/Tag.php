7fea7f60c01cd2fc8d82b2ebb151bf6bb0320dff
Tschouten95 <tschouten95@outlook.com>
2023-04-09 14:37:14 +0200

    Class and methods for tags

--- App/Controllers/Tag.php
+++ App/Controllers/Tag.php
@@ -1,11 +1,30 @@
 <?php
 
-class Tag
-{
-    public string $name;
+namespace App\Controllers;
 
-    public function __construct($name)
+use App\Controllers\FacilityController;
+
+class Tag extends FacilityController
+{
+
+    // the find function tries to find a tag with the passed through name in the database and returns the id otherwise it returns false
+    public function find($name)
     {
-        $this->name = $name;
+        $TagIDSql = "SELECT id FROM tag WHERE name = :name";
+        $TagIDSql = $this->db->connection->prepare($TagIDSql);
+        $TagIDSql->bindParam(":name", $name);
+        $TagIDSql->execute();
+        return $TagIDSql->fetchColumn();
+    }
+
+    public function create($name)
+    {
+        $TagSql = "INSERT INTO tag (name) VALUES (:name)";
+        $bind = [':name' => $name];
+        $this->db->executeQuery($TagSql, $bind);
+
+        $id = $this->db->connection->lastInsertId();
+
+        return $id;
     }
 }
